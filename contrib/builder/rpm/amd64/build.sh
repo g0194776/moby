33af43859ea00107d18827c902ca12c176c41fea
ckhitler <21488275@qq.com>
2019-11-06 22:56:16 +0800

    Update build.sh

--- contrib/builder/rpm/amd64/build.sh
+++ contrib/builder/rpm/amd64/build.sh
@@ -4,7 +4,7 @@ set -e
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 set -x
-./generate.sh
+./generate.sh centos-7
 for d in */; do
 	docker build -t "dockercore/builder-rpm:$(basename "$d")" "$d"
 done
