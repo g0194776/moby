17ad559b1c925460285b8e8a6c9f367e3b265d9b
ckhitler <21488275@qq.com>
2019-11-07 00:01:37 +0800

    Update build.sh

--- contrib/builder/rpm/amd64/build.sh
+++ contrib/builder/rpm/amd64/build.sh
@@ -7,6 +7,7 @@ set -x
 ./generate.sh centos-7
 for d in */; do
         if [[ $(basename "$d") = centos-7 ]];then
+            echo "Hack: only Centos7"
             docker build -t "dockercore/builder-rpm:$(basename "$d")" "$d"
         fi
 done
